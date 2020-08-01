eadf20c15a3ba56cb3092bf80562bddefe7089a5
ravi19999 <dahalravindra@gmail.com>
2020-08-02 01:19:44 +0545

    Added context to the templates :sparkles:

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,4 +3,26 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return render(request, 'home_page.html', {})
+    context = {
+        "title": "hello world!",
+        "content": "welcome to the home page"
+    }
+    return render(request, 'home_page.html', context)
+
+
+def about_page(request):
+    context = {
+        "title": "about world!",
+        "content": "welcome to the about page"
+
+    }
+    return render(request, 'home_page.html', context)
+
+
+def contact_page(request):
+    context = {
+        "title": "contact world!",
+        "content": "welcome to the contact page"
+
+    }
+    return render(request, 'home_page.html', context)
