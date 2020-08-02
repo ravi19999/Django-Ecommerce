85ab8ff55174875db78c53ba8371661ac1f6b50a
ravi19999 <dahalravindra@gmail.com>
2020-08-02 22:37:44 +0545

    Implimented the login feature :sparkles:

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -13,3 +13,8 @@ class ContactForm(forms.Form):
         email = self.cleaned_data.get("email")
         if not "gmail.com" in email:
             raise forms.ValidationError('email has to be gmail.com')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
 
 
 def home_page(request):
@@ -31,8 +32,34 @@ def contact_page(request):
 
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
-    # if request.method == 'POST':
-    #     print(request.POST.get('fullname'))
-    #     print(request.POST.get('email'))
-    #     print(request.POST.get('content'))
+
     return render(request, 'contact/view.html', context)
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    print(request.user.is_authenticated())
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        print(form.cleaned_data)
+        user = authenticate(request, username=username, password=password)
+        print(request.user.is_authenticated())
+        if user is not None:
+            print(request.user.is_authenticated())
+            login(request, user)
+            context['form'] = LoginForm()
+            return redirect('/login')
+        else:
+            print('Error')
+    return render(request, 'auth/login.html', context)
+
+
+def register_page(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, 'auth/register.html', {})
