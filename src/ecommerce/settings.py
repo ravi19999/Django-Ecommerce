014e653e547fa2684cb845c20756349defeaa169
ravi19999 <dahalravindra@gmail.com>
2020-08-03 13:00:00 +0545

    Set up static files :zap: :sparkles:

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -117,4 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_cdn/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_my_proj'),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(
+    BASE_DIR), "static_cdn", 'static_root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(
+    BASE_DIR), "static_cdn", 'media_root')
