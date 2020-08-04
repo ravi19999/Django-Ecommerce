1efa78e630160d9bb7473cc6a75aa1427b9b59db
ravi19999 <dahalravindra@gmail.com>
2020-08-05 01:36:57 +0545

    Added the detail view :sparkles:

--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from .models import Product
@@ -11,4 +11,15 @@ class ProductListView(ListView):
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(
             *args, **kwargs)
+        return context
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(
+            *args, **kwargs)
+
         return context
