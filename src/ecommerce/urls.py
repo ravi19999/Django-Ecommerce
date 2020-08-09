6c184a27d60610378f699f7a800b085d95010492
ravi19999 <dahalravindra@gmail.com>
2020-08-09 23:04:46 +0545

    Created a urls file for the products app :sparkles:

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -16,12 +16,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-
-from products.views import (ProductListView, ProductDetailView,
-                            ProductFeaturedDetailView, ProductDetailSlugView, ProductFeaturedListView)
-
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -30,14 +26,9 @@ urlpatterns = [
     url(r'^about/$', about_page),
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
-    url(r'^featured/$', ProductFeaturedListView.as_view()),
-    url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
-    url(r'^products/$', ProductListView.as_view()),
-    # url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
-
-    url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
     url(r'^register/$', register_page),
     url(r'^admin/', admin.site.urls),
+    url(r'products/',include("products.urls")),
 ]
 
 if settings.DEBUG:
