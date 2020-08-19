eeb91c1cae8651024e209ca69d22dc56966aef3f
ravi19999 <dahalravindra@gmail.com>
2020-08-19 23:10:21 +0545

    Pinned carts to users :pushpin:

--- src/carts/models.py
+++ src/carts/models.py
@@ -5,6 +5,16 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 
+class CartManager(models.Manager):
+    def new(self, user=None):
+        print(user)
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated():
+                user_obj = user
+            return self.model.objects.create(user=user_obj)
+
+
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True)
     products = models.ManyToManyField(Product, blank=True)
@@ -12,5 +22,7 @@ class Cart(models.Model):
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    objects = CartManager()
+
     def __str__(self):
-        return str(self.user)
+        return str(self.id)
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Cart
+
+
+def create_cart(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    return cart_obj
 
 
 def cart_home(request):
     cart_id = request.session.get('cart_id', None)
-    if cart_id is None:
-        print('create new cart')
-        request.session['cart_id'] = 12
-    else:
-        print('Cart ID exists')
 
-    return render(request, "carts/home.html", {})
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        print('Cart ID exists')
+        cart_obj = qs.first
+        if request.user.is_authenticated() and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
+    else:
+        cart_obj = Cart.objects.new(user=request.user)
+        request.session['cart_id'] = cart_obj.id
+    return render(request, 'carts/home.html')
