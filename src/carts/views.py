618df3e0a2fae4afd1ce9240c99c874470c54519
ravi19999 <dahalravindra@gmail.com>
2020-08-20 21:01:58 +0545

    Added the new_or_get model manager :recycle: :sparkles:

--- src/carts/views.py
+++ src/carts/views.py
@@ -3,22 +3,6 @@ from django.shortcuts import render
 from .models import Cart
 
 
-def create_cart(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    return cart_obj
-
-
 def cart_home(request):
-    cart_id = request.session.get('cart_id', None)
-
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        print('Cart ID exists')
-        cart_obj = qs.first
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
+    cart_obj = Cart.objects.new_or_get(request)
     return render(request, 'carts/home.html')
