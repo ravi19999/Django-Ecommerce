6c675c441b86390bc4370e5af1fefce547d55fd2
ravi19999 <dahalravindra@gmail.com>
2020-08-27 15:25:13 +0545

    Created the checkout view :sparkles: :lipstick:

--- src/billing/models.py
+++ src/billing/models.py
@@ -4,9 +4,6 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 
-# abc@teamcfe.com -->> 1000000 billing profiles
-# user abc@teamcfe.com -- 1 billing profile
-
 
 class BillingProfile(models.Model):
     user = models.OneToOneField(User, null=True, blank=True)
@@ -14,16 +11,9 @@ class BillingProfile(models.Model):
     active = models.BooleanField(default=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
-    # customer_id in Stripe or Braintree
 
     def __str__(self):
         return self.email
-
-# def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
-#     if created:
-#         print("ACTUAL API REQUEST Send to stripe/braintree")
-#         instance.customer_id = newID
-#         instance.save()
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from accounts.forms import LoginForm
+from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
 from .models import Cart
@@ -7,7 +9,7 @@ from .models import Cart
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    return render(request, "carts/home.html", {'cart': cart_obj})
+    return render(request, "carts/home.html", {"cart": cart_obj})
 
 
 def cart_update(request):
@@ -17,14 +19,16 @@ def cart_update(request):
             product_obj = Product.objects.get(id=product_id)
         except Product.DoesNotExist:
             print("Show message to user, product is gone?")
-            return redirect('cart:home')
+            return redirect("cart:home")
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
         else:
+            # cart_obj.products.add(product_id)
             cart_obj.products.add(product_obj)
         request.session['cart_items'] = cart_obj.products.count()
-    return redirect('cart:home')
+        # return redirect(product_obj.get_absolute_url())
+    return redirect("cart:home")
 
 
 def checkout_home(request):
@@ -34,5 +38,17 @@ def checkout_home(request):
         return redirect("cart:home")
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    user = request.user
+    billing_profile = None
+    login_form = LoginForm()
 
-    return render(request, "carts/checkout.html", {'object': order_obj})
+    if user.is_authenticated():
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(
+            user=user, email=user.email)
+
+    context = {
+        "object": order_obj,
+        "billing_profile": billing_profile,
+        "login_form": login_form
+    }
+    return render(request, "carts/checkout.html", context)
