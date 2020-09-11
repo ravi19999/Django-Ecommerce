3115d3b0a6e44b8e513db834348f12a22b9a2731
ravi19999 <dahalravindra@gmail.com>
2020-09-11 14:43:40 +0545

    Created an abstract base user :card_file_box:

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,12 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser)
 
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=255)
+    # full_name = models.CharField(max_length=255,blank=True,null=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def short_name(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
 
 
 class GuestEmail(models.Model):
-    email       = models.EmailField()
-    active      = models.BooleanField(default=True)
-    update      = models.DateTimeField(auto_now=True)
-    timestamp   = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    update = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
