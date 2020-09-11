b9b005aae019d71d33ca693d9f7dfa69bca6d5cc
ravi19999 <dahalravindra@gmail.com>
2020-09-11 20:03:34 +0545

    Created user model manager :card_file_box:

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,17 +1,54 @@
 from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, is_active=True, password=None, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("Users must have an email address"
+        if not password:
+            raise ValueError("Users must have a password")
+        user_obj=self.model(
+            email=self.normalize_email(email)
+        )
+        user_obj.set_password(password)
+        user_obj.staff=is_staff
+        user_obj.admin=is_admin
+        user_obj.active=is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staffuser(self, email, password=None):
+        user=self.create_user(
+            email, password=password,
+            is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, password=None):
+        user=self.create_user(
+            email, password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
 
 
 class User(AbstractBaseUser):
-    email = models.EmailField(unique=True, max_length=255)
+    email=models.EmailField(unique=True, max_length=255)
     # full_name = models.CharField(max_length=255,blank=True,null=True)
-    active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False)
-    admin = models.BooleanField(default=False)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    active=models.BooleanField(default=True)
+    staff=models.BooleanField(default=False)
+    admin=models.BooleanField(default=False)
+    timestamp=models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
+
+    objects=UserManager()
 
     def __str__(self):
         return self.email
@@ -22,24 +59,24 @@ class User(AbstractBaseUser):
     def short_name(self):
         return self.email
 
-    @property
+    @ property
     def is_staff(self):
         return self.staff
 
-    @property
+    @ property
     def is_admin(self):
         return self.admin
 
-    @property
+    @ property
     def is_active(self):
         return self.active
 
 
 class GuestEmail(models.Model):
-    email = models.EmailField()
-    active = models.BooleanField(default=True)
-    update = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    email=models.EmailField()
+    active=models.BooleanField(default=True)
+    update=models.DateTimeField(auto_now=True)
+    timestamp=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
