55f235ecce582eba633d58f0ef276e487e1403de
ravi19999 <dahalravindra@gmail.com>
2020-09-13 21:28:22 +0545

    Created the billing payment method view :sparkles:

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+import stripe
+stripe.api_key = 'sk_test_51HQwJDE9dKyATRmBfqOTCDB0lrrbYsQ402TLCd9iKBXkmrB4O7Ct3nVkjxK9HiW5Ren13woNiRCnx9lft3HX9Zr000oke1IFQH'
+STRIPE_PUB_KEY = 'pk_test_51HQwJDE9dKyATRmBYJ8cUGFJpGeL7pOdz17Pla6gVZEXRLVmjQCTaBSIuL8j96Qv8SYPwVNbuPpSPR9paqcqiUbt00LdZ9DqcB'
+
+
+def payment_method_view(request):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})
