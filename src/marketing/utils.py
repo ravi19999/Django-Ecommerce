03e3858f8100533efbb921c8667047fb455fa0cc
ravi19999 <dahalravindra@gmail.com>
2020-09-21 00:52:38 +0545

    Connected to the mailchimp API and added emails :card_file_box: :alien:

--- src/marketing/utils.py
+++ src/marketing/utils.py
@@ -1,5 +1,7 @@
+import json
 import requests
 from django.conf import settings
+
 
 MAILCHIMP_API_KEY = getattr(settings, "MAILCHIMP_API_KEY", None)
 MAILCHIMP_DATA_CENTER = getattr(settings, "MAILCHIMP_DATA_CENTER", None)
@@ -10,18 +12,33 @@ class Mailchimp(object):
     def __init__(self):
         super(Mailchimp, self).__init__()
         self.key = MAILCHIMP_API_KEY
-        self.api_url = f"https://{MAILCHIMP_DATA_CENTER}.api.mailchimp.com/3.0"
+        self.api_url = "https://{dc}.api.mailchimp.com/3.0".format(
+            dc=MAILCHIMP_DATA_CENTER
+        )
         self.list_id = MAILCHIMP_EMAIL_LIST_ID
+        self.list_endpoint = '{api_url}/lists/{list_id}'.format(
+            api_url=self.api_url,
+            list_id=self.list_id
+        )
 
-    def check_subscription_status(self, email):
+    def check_subcription_status(self, email):
         endpoint = self.api_url
         r = requests.get(endpoint, auth=("", self.key))
         return r.json()
 
+    def check_valid_status(self, status):
+        choices = ['subscribed', 'unsubscribed', 'cleaned', 'pending']
+        if status not in choices:
+            raise ValueError("Not a valid choice for email status")
+        return status
+
     def add_email(self, email):
+        status = "subscribed"
+        self.check_valid_status(status)
         data = {
-            "email": email
+            "email_address": email,
+            "status": status
         }
-        endpoint = self.api_url
-        r = requests.post(endpoint, auth=("", self.key))
+        endpoint = self.list_endpoint + "/members"
+        r = requests.post(endpoint, auth=("", self.key), data=json.dumps(data))
         return r.json()
