89d1b6e401ac388f8464d370d1e3fff1015b3cad
ravi19999 <dahalravindra@gmail.com>
2020-09-21 13:40:24 +0545

    Added methods for checking and updating subscription status :slien: :card_file_box:

--- src/marketing/utils.py
+++ src/marketing/utils.py
@@ -1,4 +1,6 @@
+import hashlib
 import json
+import re
 import requests
 from django.conf import settings
 
@@ -7,6 +9,16 @@ MAILCHIMP_API_KEY = getattr(settings, "MAILCHIMP_API_KEY", None)
 MAILCHIMP_DATA_CENTER = getattr(settings, "MAILCHIMP_DATA_CENTER", None)
 MAILCHIMP_EMAIL_LIST_ID = getattr(settings, "MAILCHIMP_EMAIL_LIST_ID", None)
 
+def check_email(email):
+    if not re.match(r".+@.+\..+", email):
+        raise ValueError("String passed is not a valid email")
+    return email
+
+def get_subscriber_hash(member_email):
+    check_email(member_email)
+    member_email = member_email.lower().encode()
+    m = hashlib.md5(member_email)
+    return m.hexdigest()
 
 class Mailchimp(object):
     def __init__(self):
@@ -21,8 +33,22 @@ class Mailchimp(object):
             list_id=self.list_id
         )
 
-    def check_subcription_status(self, email):
-        endpoint = self.api_url
+    def get_members_endpoint(self):
+        return self.list_endpoint + "/members"
+
+    def change_subscription_status(self, email,status="unsubscribed"):
+        hashed_email = get_subscriber_hash(email)
+        endpoint = self.get_members_endpoint() + "/" + hashed_email
+        data = {
+            "status": self.check_valid_status(status)
+        }
+        r = requests.put(endpoint, auth=("", self.key),data=json.dumps(data))
+        return r.json()
+
+    def check_subscription_status(self, email):
+        hashed_email = get_subscriber_hash(email)
+        print(hashed_email)
+        endpoint = self.get_members_endpoint() + "/" + hashed_email
         r = requests.get(endpoint, auth=("", self.key))
         return r.json()
 
@@ -39,6 +65,7 @@ class Mailchimp(object):
             "email_address": email,
             "status": status
         }
-        endpoint = self.list_endpoint + "/members"
+        endpoint = self.get_members_endpoint()
         r = requests.post(endpoint, auth=("", self.key), data=json.dumps(data))
         return r.json()
+
