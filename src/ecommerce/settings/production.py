58b2a04db25855ec3d5f3c2455315c633175db78
ravi19999 <dahalravindra@gmail.com>
2020-09-24 00:59:30 +0545

    Deploy :rocket;

--- src/ecommerce/settings/production.py
+++ src/ecommerce/settings/production.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
 SECRET_KEY = '@!-)jwmuzh8btr380g61=g+#&zzei&dz2(&=xbvxztady)_p(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['raviecomm.herokuapp.com']
 
