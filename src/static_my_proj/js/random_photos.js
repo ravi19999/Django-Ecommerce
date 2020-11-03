680a90f83dd8df20cda5f9674bc68beada68fb2c
ravi19999 <dahalravindra@gmail.com>
2020-11-03 10:17:56 +0545

    Created a separate JS file for fetching random images :refactor:

---
+++ src/static_my_proj/js/random_photos.js
@@ -0,0 +1,9 @@
+let randomized = Math.floor(Math.random()*10)
+	let categories = ['night-sky','darkness','night','city-lights','kathmandu-night','new-york-night','london-night','eiffel-tower-night','night-bridge','fireworks-night']
+	fetch(`https://api.unsplash.com/search/photos?page=1&query=${categories[randomized]}&client_id=DWe3Aq2S4cOfU7K_NsuagSQ_CEr1v-Ya89-7K3U5KVQ`).then((response) => response.json()).then((apple) => {let syau = apple.results[randomized].urls.regular;document.body.style.backgroundImage = `url(${syau})`;})
+
+	photoCategories = ['funny','funny-dog','funny-cat','jokes','comedy','clown','hello','namaste','greeting','thinking']
+  fetch(`https://api.unsplash.com/search/photos?page=1&query=${photoCategories[randomized]}&client_id=DWe3Aq2S4cOfU7K_NsuagSQ_CEr1v-Ya89-7K3U5KVQ`)
+  .then((response) => response.json())
+  .then((apple) => {let syau = apple.results[randomized].urls.full;
+    document.querySelector('#hello').setAttribute('src',syau);console.log(syau)})
